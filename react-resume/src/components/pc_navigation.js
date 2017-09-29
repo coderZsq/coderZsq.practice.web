6efc27cb86dc329ddc2c13399f34bdb2285280c9
coderZsq <a13701777868@yahoo.com>
2017-09-29 22:45:21 +0800

    add

--- react-resume/src/components/pc_navigation.js
+++ react-resume/src/components/pc_navigation.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import CSSModules from 'react-css-modules';
+import {animateScroll} from 'react-scroll';
 import styles from './pc_navigation.css';
 
 @CSSModules(styles)
 export default class PCNavigation extends Component {
 
-    scrollTo(x, y) {
-        window.scrollTo(x, y);
+    scrollTo(y) {
+        animateScroll.scrollTo(y);
     }
 
     render() {
@@ -18,22 +19,22 @@ export default class PCNavigation extends Component {
                             Castie!
                         </div>
                         <div styleName="items">
-                            <span onClick={this.scrollTo.bind(this, 0, 0)}>
+                            <span onClick={this.scrollTo.bind(this, 0)}>
                                 <a>Home</a>
                             </span>
-                            <span onClick={this.scrollTo.bind(this, 0, 600)}>
+                            <span onClick={this.scrollTo.bind(this, 600)}>
                                 <a>Projects</a>
                             </span>
-                            <span onClick={this.scrollTo.bind(this, 0, 1200)}>
+                            <span onClick={this.scrollTo.bind(this, 1200)}>
                                 <a>GitHub</a>
                             </span>
-                            <span onClick={this.scrollTo.bind(this, 0, 1800)}>
+                            <span onClick={this.scrollTo.bind(this, 1800)}>
                                 <a>Articles</a>
                             </span>
-                            <span onClick={this.scrollTo.bind(this, 0, 2930)}>
+                            <span onClick={this.scrollTo.bind(this, 2930)}>
                                 <a>Experience</a>
                             </span>
-                            <span onClick={this.scrollTo.bind(this, 0, 3530)}>
+                            <span onClick={this.scrollTo.bind(this, 3530)}>
                                 <a>Contact</a>
                             </span>
                         </div>
