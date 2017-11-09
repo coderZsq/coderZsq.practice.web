c4c1cdc002997312032c73ef8008d39b640009c5
coderZsq <a13701777868@yahoo.com>
2017-11-09 17:28:22 +0800

    add

---
+++ react-resume/src/javascripts/http.js
@@ -0,0 +1,19 @@
+const host = 'http://localhost:8080';
+
+export function GET(url) {
+    var fetchOptions = {
+        methos: 'GET'
+    };
+    return new Promise((resolve, reject) => {
+        fetch(url, fetchOptions)
+        .then(response => response.json())
+        .then(response => resolve(response.data))
+        .catch((error) => {
+            reject(error);
+        })
+    });
+}
+
+export const URL = {
+    fetchProfile: `${host}/portal/fetch_profile.do`
+}
