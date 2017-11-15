43a3f1b47fd4a169c0085ea86ee2c19d8340ceea
coderZsq <a13701777868@yahoo.com>
2017-11-15 09:32:48 +0800

    add

--- react-resume/src/javascripts/http.js
+++ react-resume/src/javascripts/http.js
@@ -2,7 +2,7 @@ const host = 'http://localhost:8080';
 
 export function GET(url) {
     var fetchOptions = {
-        methos: 'GET'
+        method: 'GET'
     };
     return new Promise((resolve, reject) => {
         fetch(url, fetchOptions).then(response => response.json()).then(response => resolve(response.data)).catch((error) => {
