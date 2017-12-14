757ed56b8d2b5fe16d51db8a2207fa545ddc697a
coderZsq <a13701777868@yahoo.com>
2017-12-14 16:53:19 +0800

    add

--- react-resume/src/components/admin_home.js
+++ react-resume/src/components/admin_home.js
@@ -15,18 +15,19 @@ export default class AdminHome extends Component {
                 profileCareer: '',
                 profileLocation: '',
                 profileSocialList: []
-            }
+            },
+            profile_social: ''
         }
     }
 
     componentWillMount() {
         GET(URL.fetchProfile).then((data) => {
             this.setState({data: data})
-            this.backfill(data);
+            this.refsFillBack(data);
         })
     }
 
-    backfill(data) {
+    refsFillBack(data) {
         let refs = this.refs;
         refs.profile_image.value = data.profileImage;
         refs.profile_name.value = data.profileName;
@@ -36,6 +37,10 @@ export default class AdminHome extends Component {
         refs.profile_summary_description.value = data.profileSummaryDescription;
         refs.profile_interest_title.value = data.profileInterestTitle;
         refs.profile_education_title.value = data.profileEducationTitle;
+        for (var index = 0, length = data.profileSocialList.length; index < length; index++) {
+            refs[`src_${index}`].value = data.profileSocialList[index].src;
+            refs[`href_${index}`].value = data.profileSocialList[index].href;
+        }
     }
 
     render() {
@@ -48,6 +53,21 @@ export default class AdminHome extends Component {
                 <a href={socialVo.href}><img src={socialVo.src} alt=""/></a>
             </div>
         ))
+        let profile_social = data.profileSocialList.map((socialVo, index) => {
+            let src = `src_${index}`;
+            let href = `href_${index}`;
+            return (
+                <div key={index}>
+                    <div styleName="form-line">
+                        <span>src:</span>
+                        <input type="text" ref={src} autoComplete="off"/>
+                        <span>href:</span>
+                        <input type="text" ref={href} autoComplete="off"/>
+                    </div>
+                </div>
+            )
+        })
+
         return (
             <div>
                 <div styleName="user">
@@ -74,36 +94,39 @@ export default class AdminHome extends Component {
                     <div styleName="profile-block">
                         <div styleName="form-line">
                             <span>profile_image:</span>
-                            <input type="text" ref="profile_image" autocomplete="off"/>
+                            <input type="text" ref="profile_image" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_name:</span>
-                            <input type="text" ref="profile_name" autocomplete="off"/>
+                            <input type="text" ref="profile_name" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_career:</span>
-                            <input type="text" ref="profile_career" autocomplete="off"/>
+                            <input type="text" ref="profile_career" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_location:</span>
-                            <input type="text" ref="profile_location" autocomplete="off"/>
+                            <input type="text" ref="profile_location" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_summary_title:</span>
-                            <input type="text" ref="profile_summary_title" autocomplete="off"/>
+                            <input type="text" ref="profile_summary_title" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_summary_description:</span>
-                            <input type="text" ref="profile_summary_description" autocomplete="off"/>
+                            <input type="text" ref="profile_summary_description" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_interest_title:</span>
-                            <input type="text" ref="profile_interest_title" autocomplete="off"/>
+                            <input type="text" ref="profile_interest_title" autoComplete="off"/>
                         </div>
                         <div styleName="form-line">
                             <span>profile_education_title:</span>
-                            <input type="text" ref="profile_education_title" autocomplete="off"/>
+                            <input type="text" ref="profile_education_title" autoComplete="off"/>
                         </div>
+                    </div>
+                    <div styleName="profile_social-block">
+                        {profile_social}
                     </div>
                 </div>
             </div>
