358665bc4f8fc5d0734aca4cb3f40e7d3321b70a
coderZsq <a13701777868@yahoo.com>
2019-04-04 13:05:52 +0800

    add

---
+++ study-notes/react-native/HomePage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render(): React.ReactNode {
+        return <View>
+            <Text>123</Text>
+        </View>;
+    }
+}
