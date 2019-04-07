78d085bec453fa12f6bf688a331ff4e2e1c06695
coderZsq <a13701777868@yahoo.com>
2019-04-07 12:53:30 +0800

    add

---
+++ study-notes/flutter/layout/lib/src/layout2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
