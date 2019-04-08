3bde543fdde4c14bbe047ed8d68dff7df224d2c7
coderZsq <a13701777868@yahoo.com>
2019-04-08 23:29:16 +0800

    add

--- study-notes/flutter/animation/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'src/animation2.dart';
-
-void main() => runApp(MyApp());
-
--- study-notes/flutter/interactive/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'src/interactive3.dart';
-
-void main() => runApp(MyApp());
