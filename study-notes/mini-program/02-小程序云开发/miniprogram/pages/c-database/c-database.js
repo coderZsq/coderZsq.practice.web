38bba69ecf5b8884bd8bddf8ae1ac09c6d0a4b9f
coderZsq <a13701777868@yahoo.com>
2019-11-26 11:20:05 +0800

    add

--- study-notes/mini-program/02-小程序云开发/miniprogram/pages/c-database/c-database.js
+++ study-notes/mini-program/02-小程序云开发/miniprogram/pages/c-database/c-database.js
@@ -6,7 +6,12 @@ const db = wx.cloud.database();
 // 2.获取操作的集合
 const collection = db.collection('students');
 
+const LIMIT = 3;
+
 Page({
+  data: {
+    page: 0
+  },
   addDataToDB: function () {
     // 向集合中添加一条数据
     collection.add({
@@ -103,7 +108,76 @@ Page({
     //   .then(console.log)
     //   .catch(console.log);
   },
-  queryDataToDB: function () {
+  queryDataFromDB: function () {
+    // 1. 根据id精准查找数据
+    collection
+      .doc('05a1947c5dd882c0023c977968c93c6d')
+      .get()
+      .then(res => {
+        console.log(res);
+      });
 
+    // 2. 根据条件查找where
+    collection
+      .where({ age: 30 })
+      .get()
+      .then(res => {
+        console.log(res);
+      });
+
+    // 3. 使用查询指令数据查询
+    const cmd = db.command;
+    collection
+      .where({ age: cmd.gte(30) })
+      .get()
+      .then(res => {
+        console.log(res);
+      });
+
+    // 4. 根据正则表达式获取数据
+    collection
+      .where({
+        name: db.RegExp({
+          regexp: '^li.*',
+          options: 'i'
+        })
+      })
+      .get()
+      .then(res => {
+        console.log(res);
+      });
+
+    // 5. 不跟任何的条件, 直接查询整个集合
+    collection
+      .get()
+      .then(res => {
+        console.log(res);
+      });
+
+    // 6. 几个字段的作用
+    // field: 过滤只需要获取的字段
+    collection
+      .field({
+        name: true,
+        age: true,
+        height: true
+      })
+      .get()
+      .then(res => {
+        console.log(res);
+      });
+
+    // skip: 跳过多少条数据
+    // limit: 本次获取多少条数据
+    // orderBy:
+    db.collection('wzry')
+      .skip(this.data.page * LIMIT)
+      .limit(LIMIT)
+      .orderBy('rid', 'asc')
+      .get()
+      .then(res => {
+        this.data.page += 1;
+        console.log(res);
+      });
   }
 })
