4622398055ea123c40d7e94db5a1cc0dd5e27c84
coderZsq <a13701777868@yahoo.com>
2019-12-05 14:45:54 +0800

    add

---
+++ study-notes/architecture/02-前端架构BFF/03-express源码分析/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+// 设置使用ejs渲染
+app.set('view engine', 'ejs')
+
+// 1. 处理路由的时候, 我们需要对不同的请求做不同的处理
+app.get('/test', function (req, res) {
+  res.send('express test')
+})
+
+// 2. 我们可以给路由中设置参数
+app.get('/github/:username', function (req, res) {
+  console.log(req.params.username)
+  res.send('success')
+})
+
+// 3. 处理静态资源
+app.use(express.static('./public'))
+
+// 4. express处理后台渲染
+app.get('/ejsTest', function (req, res) {
+  res.render('index.ejs', { a: 9 })
+})
+
+app.listen(3000)
