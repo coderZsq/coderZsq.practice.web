6da2c0ba415a656a4838eff3bd37377511c500c7
coderZsq <a13701777868@yahoo.com>
2019-12-10 10:47:18 +0800

    add

--- study-notes/architecture/03-前端工程化/03-ast抽象语法树/ast4.js
+++ study-notes/architecture/03-前端工程化/03-ast抽象语法树/ast4.js
@@ -6,7 +6,18 @@ const babel = require('babel-core')
 const types = require('babel-types')
 
 let visitor = {
+  ImportDeclaration(path) {
+    let node = path.node
+    let specifiers = node.specifiers
+    if (!types.isImportDefaultSpecifier(specifiers[0])) {
+      // 如果不是默认导入, 我们需要创建自己的Declaration
+      let newImports = specifiers.map(specifier => (
+        types.importDeclaration([types.importDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
+      ))
 
+      path.replaceWithMultiple(newImports)
+    }
+  }
 }
 
 let code = `import { flatten, join } from 'lodash'`
@@ -14,3 +25,9 @@ let code = `import { flatten, join } from 'lodash'`
 let result = babel.transform(code, {
   plugins: [{ visitor }]
 })
+
+module.exports = function () {
+  return visitor
+}
+
+console.log(result.code)
