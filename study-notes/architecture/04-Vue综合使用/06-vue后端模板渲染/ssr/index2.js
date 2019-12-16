b3ed2db6d4b670f6cbffc8ad10acbeddd722b0ea
coderZsq <a13701777868@yahoo.com>
2019-12-16 17:15:42 +0800

    add

---
+++ study-notes/architecture/04-Vue综合使用/06-vue后端模板渲染/ssr/index2.js
@@ -0,0 +1,28 @@
+const Vue = require('vue')
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer()
+
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>URL IS: {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
+})
+
+server.listen(8080)
