1a040bbf753edc6a99d0a97c27a42c7788b0c0e9
coderZsq <a13701777868@yahoo.com>
2019-12-25 15:00:33 +0800

    add

--- study-notes/engineering/03-React/06-React实战/blog/components/Advert.js
+++ study-notes/engineering/03-React/06-React实战/blog/components/Advert.js
@@ -3,10 +3,7 @@ import '../public/style/components/advert.css'
 const Advert = () => {
   return (
     <div className="comm-box ad">
-      <div><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577858609&di=1cfe8e22ac39ac22219b6aa91957e388&imgtype=jpg&er=1&src=http%3A%2F%2F58pic.ooopic.com%2F58pic%2F12%2F81%2F77%2F67V58PICtyp.jpg" width="100%" /></div>
-      <div><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577858609&di=1cfe8e22ac39ac22219b6aa91957e388&imgtype=jpg&er=1&src=http%3A%2F%2F58pic.ooopic.com%2F58pic%2F12%2F81%2F77%2F67V58PICtyp.jpg" width="100%" /></div>
-      <div><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577858609&di=1cfe8e22ac39ac22219b6aa91957e388&imgtype=jpg&er=1&src=http%3A%2F%2F58pic.ooopic.com%2F58pic%2F12%2F81%2F77%2F67V58PICtyp.jpg" width="100%" /></div>
-      <div><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577858609&di=1cfe8e22ac39ac22219b6aa91957e388&imgtype=jpg&er=1&src=http%3A%2F%2F58pic.ooopic.com%2F58pic%2F12%2F81%2F77%2F67V58PICtyp.jpg" width="100%" /></div>
+      <div><img src="https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2864964483,1965654912&fm=26&gp=0.jpg" width="100%" /></div>
     </div>
   )
 }
--- study-notes/engineering/03-React/06-React实战/blog/pages/detailed.js
+++ study-notes/engineering/03-React/06-React实战/blog/pages/detailed.js
@@ -1,23 +1,101 @@
 import React from 'react'
 import Head from 'next/head'
-import { Row, Col } from 'antd'
+import { Row, Col, Icon, Breadcrumb, Affix } from 'antd'
 import Header from '../components/Header'
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import '../public/style/pages/detailed.css'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
 
-const Detailed = () => (
-  <div>
-    <Head>
-      <title>Detailed</title>
-    </Head>
-    <Header />
-    <Row className="comm-main" type="flex" justify="center">
-      <Col className="comm-left" xs={24} sm={24} md={18} lg={16} xl={14}>
-        左侧
-      </Col>
-      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-        右侧
-      </Col>
-    </Row>
-  </div>
-)
+const Detailed = () => {
+  let markdown = '# P01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n' +
+    '\`console.log(111)\` \n\n' +
+    '# p02:来个Hello World 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n' +
+    '***\n\n\n' +
+    '# p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '#5 p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '``` var a=11; ```'
+  return (
+    <div>
+      <Head>
+        <title>Detailed</title>
+      </Head>
+      <Header />
+      <Row className="comm-main" type="flex" justify="center">
+        <Col className="comm-left" xs={24} sm={24} md={18} lg={16} xl={14}>
+          <div className="bread-div">
+            <Breadcrumb>
+              <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+              <Breadcrumb.Item><a href="/">视频列表</a></Breadcrumb.Item>
+              <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div>
+            <div className="detailed-title">
+              React实战视频教程-Blog开发 (更新第10集)
+          </div>
+            <div className="list-icon center">
+              <span><Icon type="calendar" />2019-06-28</span>
+              <span><Icon type="folder" />视频教程</span>
+              <span><Icon type="fire" />5498人</span>
+            </div>
+            <div className="detailed-content">
+              <ReactMarkdown
+                source={markdown}
+                escapeHtml={false}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
+            <div className="detailed-nav comm-box">
+              <div className="nav-title">文章目录</div>
+              <MarkNav
+                className="article-menu"
+                source={markdown}
+                ordered={false}
+              />
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+      <Footer />
+    </div>
+  )
+}
 
 export default Detailed
+
