453049a4baf08008ba629bb8bc7fd79267a10190
coderZsq <a13701777868@yahoo.com>
2020-03-05 23:32:06 +0800

    add

--- study-notes/ethereum/solidity/Car.sol
+++ study-notes/ethereum/solidity/Car.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.1;
 contract Car {
     string brand;
     uint256 public price;
+    enum Weekday { Monday, Tuesday, Wednesday }
     constructor(string memory initBrand, uint256 initPrice) public {
         brand = initBrand;
         price = initPrice;
@@ -15,5 +16,18 @@ contract Car {
     }
     function setPrice(uint256 newPrice) public {
         price = newPrice;
+    }
+    function test() public pure returns(uint16) {
+        // bytes17 a;
+        // return a.length;
+        Weekday wd = Weekday.Wednesday;
+        return uint16(wd);
+    }
+}
+contract CarPayable {
+    string a;
+    Car car = new Car("BMW", 10000);
+    fallback() external {
+        address(this).balance;
     }
 }
