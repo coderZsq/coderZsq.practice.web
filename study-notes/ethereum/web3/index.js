1d142cc34a4b5dfaf6b542b3ce3260e8aa905b30
coderZsq <a13701777868@yahoo.com>
2020-03-06 21:48:19 +0800

    add

--- study-notes/ethereum/web3/index.js
+++ study-notes/ethereum/web3/index.js
@@ -1,3 +1,16 @@
-var Web3 = require("web3")
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+var Web3 = require('web3')
+var BigNumber = require('bignumber.js')
+
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 console.log(web3.isConnected())
+
+var balance = new BigNumber('1234567890123456789012345678901234567890')
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toString(10))
+console.log(balance.toString(16))
+console.log(balance.toString(2))
+
+balance = new BigNumber('1234567890.123456789012345678901234567890')
+console.log(balance)
+console.log(balance.toString(10))
