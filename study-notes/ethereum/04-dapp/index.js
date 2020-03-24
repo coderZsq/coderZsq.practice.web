7ccb3cc73eed98fbe6004a55b2785b0158d35769
coderZsq <a13701777868@yahoo.com>
2020-03-24 18:19:15 +0800

    add

--- study-notes/ethereum/04-dapp/index.js
+++ study-notes/ethereum/04-dapp/index.js
@@ -1,11 +1,28 @@
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
-var abi = JSON.parse()
-var contractAddr = ""
+var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidateName","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidateName","type":"bytes32"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"candidateListName","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+var contractAddr = "0x6b5ed6cd17190b88ff1bf4a419a69877f091c98d"
 var VotingContract = web3.eth.contract(abi)
 var contractInstance = VotingContract.at(contractAddr)
 
 var candidates = { "Alice": "candidate-1", "Bob": "candidate-2", "Cary": "candidate-3" }
+
+function voteForCandidate() {
+  let candidateName = $("#candidate").val()
+  try {
+    contractInstance.vote(candidateName, { from: web3.eth.accounts[0] }, (err, res) => {
+      if (err) {
+        console.log("Error: ", err)
+      } else {
+        let id = candidates[candidateName]
+        let count = contractInstance.totalVotesFor(candidateName).toString()
+        $("#" + id).html(count)
+      }
+    })
+  } catch (err) {
+
+  }
+}
 
 $(document).ready(function () {
   var candidateList = Object.keys(candidates)
