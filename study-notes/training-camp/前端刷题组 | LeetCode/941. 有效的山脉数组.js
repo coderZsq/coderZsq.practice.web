ab54f64c3dd3a579a72a2b08621b9f5aa18a7a53
coderZsq <a13701777868@yahoo.com>
2020-11-03 14:07:35 +0800

    add

--- study-notes/training-camp/前端刷题组 | LeetCode/941. 有效的山脉数组.js
+++ study-notes/training-camp/前端刷题组 | LeetCode/941. 有效的山脉数组.js
@@ -9,6 +9,6 @@
     let right = len - 1
     while (left + 1 < len && A[left] < A[left + 1]) left++
     while (right > 0 && A[right - 1] > A[right]) right--
-    return left > 0 && right < len - 1 && left == right;
+    return left > 0 && right < len - 1 && left == right
   }
 }
