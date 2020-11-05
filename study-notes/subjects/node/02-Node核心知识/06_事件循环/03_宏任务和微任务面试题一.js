e40663309484316f363c75db0291d86d7e1806d9
coderZsq <a13701777868@yahoo.com>
2020-11-05 18:03:04 +0800

    add

---
+++ study-notes/subjects/node/02-Node核心知识/06_事件循环/03_宏任务和微任务面试题一.js
@@ -0,0 +1,47 @@
+setTimeout(function () {
+  console.log('set1')
+
+  new Promise(function (reslove) {
+    reslove()
+  }).then(function () {
+    new Promise(function (reslove) {
+      reslove()
+    }).then(function () {
+      console.log('then4')
+    })
+    console.log('then2')
+  })
+})
+
+new Promise(function (resolve) {
+  console.log('pr1')
+  resolve()
+}).then(function () {
+  console.log('then1')
+})
+
+setTimeout(function () {
+  console.log('set2')
+})
+
+console.log(2)
+
+queueMicrotask(() => {
+  console.log('queueMicrotask1')
+})
+
+new Promise(function (reslove) {
+  reslove()
+}).then(function () {
+  console.log('then3')
+})
+
+// pr1
+// 2
+// then1
+// queueMicrotask1
+// then3
+// set1
+// then2
+// then4
+// set2
