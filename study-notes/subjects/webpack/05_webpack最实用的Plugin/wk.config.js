e0bdca6f3c456485eeb432e04aba49e4c68c4ee3
coderZsq <a13701777868@yahoo.com>
2021-02-03 14:31:39 +0800

    add

---
+++ study-notes/subjects/webpack/05_webpack最实用的Plugin/wk.config.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'js/bundle.js',
+    // 必须是一个绝对路径
+    path: path.resolve(__dirname, './build'),
+    // assetModuleFilename: 'img/[name].[hash:6][ext]'
+  },
+  module: {
+    rules: [
+      {
+        // 规则使用正则表达式
+        test: /\.css$/, // 匹配资源
+        use: [
+          // { loader: 'css-loader' },
+          // 注意: 编写顺序 (从下往上, 从右往左, 从后往前)
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: [
+          //         // require('autoprefixer'),
+          //         // require('postcss-preset-env')
+          //         'postcss-preset-env'
+          //       ]
+          //     }
+          //   }
+          // }
+        ]
+        // loader: 'css-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'postcss-loader',
+          'less-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        // type: 'asset/resource', // file-loader的效果
+        // type: 'asset/inline', // url-loader
+        type: 'asset',
+        generator: {
+          filename: 'img/[name].[hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024
+          }
+        }
+      },
+      {
+        test: /\.ttf|eot|woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name].[hash:6][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'coderZsq webpack',
+      template: './public/index.html'
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: [
+              '**/index.html',
+              '**/.DS_Store',
+              '**/abc.txt'
+            ]
+          }
+        }
+      ]
+    })
+  ]
+}
