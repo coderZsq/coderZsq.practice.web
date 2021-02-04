d0e5f2ac9d9ac4605de5269b65b2a74956bc3a05
coderZsq <a13701777868@yahoo.com>
2021-02-04 11:36:13 +0800

    add

---
+++ study-notes/subjects/webpack/06_webpack模块化原理/src/common_index.js
@@ -0,0 +1,4 @@
+const { dateFormat, priceFormat } = require('./js/format');
+
+console.log(dateFormat('abc'));
+console.log(priceFormat('abc'));
---
+++ study-notes/subjects/webpack/06_webpack模块化原理/src/js/format.js
@@ -0,0 +1,12 @@
+const dateFormat = (date) => {
+    return "2020-12-12";
+}
+
+const priceFormat = (price) => {
+    return "100.00";
+}
+
+module.exports = {
+    dateFormat,
+    priceFormat
+}
