6f3420622447a07bba6902750490437c4be8a0ac
coderZsq <a13701777868@yahoo.com>
2021-02-05 13:42:07 +0800

    add

--- study-notes/subjects/webpack/12_webpack加载vue文件/result/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var message = 'Hello World';
-
-var foo = function foo(info) {
-  console.log(info);
-};
-
-foo(message);
--- study-notes/subjects/webpack/12_webpack加载vue文件/wk.config.js
+++ study-notes/subjects/webpack/12_webpack加载vue文件/wk.config.js
@@ -1,24 +1,39 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
   output: {
-    filename: 'js/bundle.js',
+    filename: "js/bundle.js",
     // 必须是一个绝对路径
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, "./build"),
     // assetModuleFilename: 'img/[name].[hash:6][ext]'
   },
   module: {
     rules: [
       {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          "postcss-loader",
+          "less-loader",
+        ],
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           // options: {
           //   presets: [
           //     ['@babel/preset-env', {
@@ -30,22 +45,27 @@ module.exports = {
           //   //   '@babel/plugin-transform-block-scoping'
           //   // ]
           // }
-        }
+        },
       },
       {
         test: /\.ts$/,
         exclude: /node_modules/,
         // 本质上是依赖于typescript (typescript compiler)
         // use: 'ts-loader'
-        use: 'babel-loader'
-      }
-    ]
+        use: "babel-loader",
+      },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'coderZsq webpack',
-      template: './index.html'
-    })
-  ]
-}
+      title: "coderZsq webpack",
+      template: "./index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
