369945654ef9d3a047f9e2ac12ecdf5252951b57
coderZsq <a13701777868@yahoo.com>
2021-05-24 17:39:25 +0800

    add

---
+++ study-notes/subjects/vue/10_组件化开发/component-a.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ desc }}</p>
+    <button @click="btnClick">按钮点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '我是标题',
+      desc: '我是内容, 哈哈哈哈哈',
+    };
+  },
+  methods: {
+    btnClick() {
+      console.log('按钮的点击');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
