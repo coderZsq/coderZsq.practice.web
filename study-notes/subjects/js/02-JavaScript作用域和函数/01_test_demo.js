48d83b4ca2a023b79d3b1fad23c59a2f8afca6cf
coderZsq <a13701777868@yahoo.com>
2021-08-31 12:15:17 +0800

    add

---
+++ study-notes/subjects/js/02-JavaScript作用域和函数/01_test_demo.js
@@ -0,0 +1,17 @@
+// const name = 'sq';
+// console.log(name);
+
+// function foo() {
+//   console.log('foo');
+// }
+
+// foo();
+
+// function outer() {
+//   function inner() {
+//     var inner = 'inner';
+//     console.log(inner);
+//   }
+// }
+
+// outer();
